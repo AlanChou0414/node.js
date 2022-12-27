09391ec52a3bd39d94d17baaf53caabf7bfd5b10
AlanChou1995 <99056343+AlanChou1995@users.noreply.github.com>
2022-12-27 11:41:11 +0800

    create server in node.js

---
+++ http_server01.js
@@ -0,0 +1,20 @@
+// http is node.js default function;
+const http = require('http');
+
+// (request , response) can't change location;
+const server = http.createServer((req, res) => {
+    //status code 200
+    res.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+
+    //response done => write content to browser;
+    res.end(`
+        <h1>Hello</h1>
+        <p>${req.url}</p>
+    `);
+});
+
+
+// localhost:port
+server.listen(3000);
