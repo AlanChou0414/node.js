6dd5fa22d802d3ff1090fefc45e313b96785e6dc
AlanChou1995 <99056343+AlanChou1995@users.noreply.github.com>
2022-12-27 10:21:56 +0800

    module exports => require

---
+++ src/try-person.js
@@ -0,0 +1,7 @@
+const Person = require('./person');
+const p1 = new Person('David',25);
+const p2 = new Person('Alan',27);
+const p3 = new Person('Kevin',30);
+console.log(p1.toString());
+console.log(p2.toString());
+console.log(p3.toString());
