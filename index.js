48e93f8c3e0c20ba7f3b940aed2bab31cc8a3a0a
AlanChou1995 <99056343+AlanChou1995@users.noreply.github.com>
2022-12-29 10:57:04 +0800

    router baseurl

--- index.js
+++ index.js
@@ -117,7 +117,8 @@ app.get(/\/m\/09\d{2}-?\d{3}-?\d{3}$/i, (req, res) => {
   res.send({ u });
 });
 
-app.use(require('./routes/admin2'));
+// app.use(require('./routes/admin2'));
+app.use('/admins', require('./routes/admin2'));
 
 /*---------------------------------
 app.get('/a.html',(req,res) => {
--- routes/admin2.js
+++ routes/admin2.js
@@ -1,3 +1,4 @@
+// Router modular;
 const express = require('express');
 
 const router = express.Router();
