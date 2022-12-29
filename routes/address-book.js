2a3716260be6fe1238249032a840eea8455ea3b2
AlanChou1995 <99056343+AlanChou1995@users.noreply.github.com>
2022-12-29 15:23:56 +0800

    add user page function

--- routes/address-book.js
+++ routes/address-book.js
@@ -5,9 +5,14 @@ const db = require('../modules/connect-mysql');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-  const [rows] = await db.query('SELECT * FROM products LIMIT 5');
+  const page = +req.query.page || 1; // which pages does user looking at;
 
-  res.json(rows);
+  const perPage = 20;
+  const tSql = 'SELECT COUNT(1) totalRows FROM address_book';
+  const [[{ totalRows }]] = await db.query(tSql);
+  const totalPages = Math.ceil(totalRows / perPage);
+
+  res.json({ totalRows, totalPages, page });
 });
 
 module.exports = router;
